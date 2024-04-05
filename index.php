a2bd4c7bbbf80486372784769a3af6a3f28607b4
Omar Fandiño <omarfandinop@gmail.com>
2024-04-05 12:11:31 -0500

    test .env

--- index.php
+++ index.php
@@ -6,11 +6,14 @@ use Slim\Factory\AppFactory;
 
 require __DIR__ . '/vendor/autoload.php';
 
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
 $app = AppFactory::create();
 
 $app->get('/', function (Request $request, Response $response) {
 	echo $_ENV['OMAR'];
-	$response->getBody()->write('Hello World');
+	$response->getBody()->write(' Hello World');
 	return $response;
 });
 
